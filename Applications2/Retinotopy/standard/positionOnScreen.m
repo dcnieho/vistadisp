71444453e6c70a46dfec73fdbeeebba96841b2ee
Diederick Niehorster <dcnieho@gmail.com>
2020-07-01 15:06:22 +0200

    done, can now move mapping stimulus before scan start

--- Applications2/Retinotopy/standard/positionOnScreen.m
+++ Applications2/Retinotopy/standard/positionOnScreen.m
@@ -1,5 +1,4 @@
 function destRect = positionOnScreen(params)
-params_display = params.display;
 
 % get stim to show
 img = makeRetinotopyWholeStim(params);
@@ -9,7 +8,49 @@ srcRect = [0,0,size(img, 2), size(img, 1)];
 destRect = CenterRect(srcRect, params.display.rect);
 
 % display, listen for key press, until done
+stepSz = 10;
 while true
-    break;
+    % show 
+    Screen('DrawTexture', params.display.windowPtr, tex, [], destRect);
+    Screen('Flip', params.display.windowPtr);
+    
+    % listen to keyboard
+    [~,keyCode] = KbWait([],3);
+    if any(keyCode)
+        keys = KbName(keyCode);
+        if ~iscell(keys), keys = {keys}; end
+        if any(cellfun(@(x) ~isempty(strfind(lower(x(1:min(2,end))),'up')),keys)) %#ok<STREMP>
+            % up arrow key (test so round-about
+            % because KbName could return both 'up'
+            % and 'UpArrow', depending on platform
+            % and mode)
+            if destRect(2)>=stepSz
+                destRect = OffsetRect(destRect,0,-stepSz);
+            end
+        elseif any(cellfun(@(x) ~isempty(strfind(lower(x(1:min(4,end))),'down')),keys)) %#ok<STREMP>
+            % down key
+            if destRect(4)<=params.display.rect(4)-stepSz
+                destRect = OffsetRect(destRect,0,stepSz);
+            end
+        end
+        if any(cellfun(@(x) ~isempty(strfind(lower(x(1:min(4,end))),'left')),keys)) %#ok<STREMP>
+            if destRect(1)>=stepSz
+                destRect = OffsetRect(destRect,-stepSz,0);
+            end
+        elseif any(cellfun(@(x) ~isempty(strfind(lower(x(1:min(5,end))),'right')),keys)) %#ok<STREMP>
+            % right key
+            if destRect(3)<=params.display.rect(3)-stepSz
+                destRect = OffsetRect(destRect,stepSz,0);
+            end
+        end
+        
+        
+        % check for done
+        if any(strcmpi(keys,'escape')) || any(strcmpi(keys,'space'))
+            break;
+        end
+    end
 end
-destRect = OffsetRect(destRect,-500,500);
+
+% clear screen
+Screen('Flip', params.display.windowPtr);
