732db7662c7b5b6884f52a961e624753016a4105
Diederick Niehorster <dcnieho@gmail.com>
2021-02-02 13:43:20 +0100

    bit of cleanup

--- Applications2/Retinotopy/standard/retHardcoded.m
+++ Applications2/Retinotopy/standard/retHardcoded.m
@@ -5,12 +5,6 @@ function [params,response,timing] = retHardcoded
 %     params: optional input argument to specify experiment parameters. if
 %             omitted, GUI is opened to set parameters
 %
-% 06/2005 SOD Ported to OSX. If the mouse is invisible,
-%             moving it to the Dock usually make s it reappear.
-% 10/2005 SOD Several changes, including adding gui.
-% 1/2009 JW   Added optional input arg 'params'. This allows you to
-%             specify your parameters in advance so that the GUI loads up
-%             with the values you want. 
 % 
 % Examples:
 %
@@ -25,11 +19,6 @@ function [params,response,timing] = retHardcoded
 %   % modify fields as you like, e.g.
 %   params.fixation = 'dot';
 %   ret(params)
-
-% TODO:
-% - stimulus moving
-% - get rid of resolution changing on open and close
-% - 
 
 homeDir = fullfile(fileparts(mfilename('fullpath')),'..','..','..');
 addpath(genpath(homeDir))
